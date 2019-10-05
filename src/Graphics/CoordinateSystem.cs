9e69f03209c4f5db32b6349c5687050bdb7a6c09
zjwilliams42 <zjwilliams42@gmail.com>
2019-10-04 20:39:12 -0500

    Testing out a different grid.
    This update was an attempt at getting a more usable grid implemented.
    The new grid does look better, and uses guide lines with the aability to
    draw/not draw by quadrent. The issue is, the vector will not overlay it
    correctly.
    
    My best guess this that each quadrent will need to control the drawing
    of the vectors inside of it, but I don't know if I am willing to
    implement that. I may, I may not. That's why it's a branch.

--- src/Graphics/CoordinateSystem.cs
+++ src/Graphics/CoordinateSystem.cs
@@ -24,6 +24,93 @@ namespace Physics_Sim
                 );
             }
 
+            private void DrawQuadrent (BasicEffect effect, int x, int y, int z, bool grid = false)
+            {
+
+                VertexPositionColor[] vectorVertices;
+                vectorVertices = new VertexPositionColor[Math.Abs(x) * 4 * 3 + 6];
+                
+                if (!grid)
+                { 
+                    vectorVertices[0] = new VertexPositionColor( new Vector3(0, 0, 0), Color.Black );
+                    vectorVertices[1] = new VertexPositionColor( new Vector3(x, 0, 0), Color.Black );
+                    vectorVertices[2] = new VertexPositionColor( new Vector3(0, 0, 0), Color.Black );
+                    vectorVertices[3] = new VertexPositionColor( new Vector3(0, y, 0), Color.Black );
+                    vectorVertices[4] = new VertexPositionColor( new Vector3(0, 0, 0), Color.Black );
+                    vectorVertices[5] = new VertexPositionColor( new Vector3(0, 0, z), Color.Black );
+
+                    foreach (var pass in effect.CurrentTechnique.Passes)
+                    {
+                        pass.Apply ();
+
+                        graphics.GraphicsDevice.DrawUserPrimitives (
+                            PrimitiveType.LineList,
+                            vectorVertices,
+                            0,
+                            3
+                        );
+                    }
+                }
+                else
+                {
+                    int j = 0;
+                    int i = 0;
+
+                    int x_sign = (x >= 0) ? 1 : -1;
+                    int y_sign = (y >= 0) ? 1 : -1;
+                    int z_sign = (z >= 0) ? 1 : -1;
+                    for (i =x_sign; Math.Abs(i) <= Math.Abs(x); i+=x_sign)
+                    {
+                        // x-z axis
+                        vectorVertices[j++] = new VertexPositionColor( new Vector3(i, 0, 0), Color.LightBlue );
+                        vectorVertices[j++] = new VertexPositionColor( new Vector3(i, 0, z), Color.LightBlue );
+                        vectorVertices[j++] = new VertexPositionColor( 
+                            new Vector3(0, 0, z_sign * Math.Abs(i)), Color.LightBlue );
+                        vectorVertices[j++] = new VertexPositionColor( 
+                            new Vector3(x_sign * Math.Abs(z), 0, z_sign * Math.Abs(i)), Color.LightBlue );
+
+                        // x-y axis
+                        vectorVertices[j++] = new VertexPositionColor( 
+                            new Vector3(0, y_sign * Math.Abs(i), 0), Color.LightBlue );
+                        vectorVertices[j++] = new VertexPositionColor( 
+                            new Vector3(0, y_sign * Math.Abs(i), z), Color.LightBlue );
+                        vectorVertices[j++] = new VertexPositionColor(
+                            new Vector3(0, 0, z_sign * Math.Abs(i)), Color.LightBlue );
+                        vectorVertices[j++] = new VertexPositionColor( 
+                            new Vector3(0, y_sign * Math.Abs(z), z_sign * Math.Abs(i)), Color.LightBlue );
+
+                        // z-y axis
+                        vectorVertices[j++] = new VertexPositionColor( 
+                            new Vector3(0, y_sign * Math.Abs(i), 0), Color.LightBlue );
+                        vectorVertices[j++] = new VertexPositionColor( 
+                            new Vector3(x, y_sign * Math.Abs(i), 0), Color.LightBlue );
+                        vectorVertices[j++] = new VertexPositionColor( new Vector3(i, 0, 0), Color.LightBlue );
+                        vectorVertices[j++] = new VertexPositionColor( 
+                            new Vector3(i, y_sign * Math.Abs(x), 0), Color.LightBlue );
+                    }
+
+                    vectorVertices[j++] = new VertexPositionColor( new Vector3(0, 0, 0), Color.Black );
+                    vectorVertices[j++] = new VertexPositionColor( new Vector3(x, 0, 0), Color.Black );
+                    vectorVertices[j++] = new VertexPositionColor( new Vector3(0, 0, 0), Color.Black );
+                    vectorVertices[j++] = new VertexPositionColor( new Vector3(0, y, 0), Color.Black );
+                    vectorVertices[j++] = new VertexPositionColor( new Vector3(0, 0, 0), Color.Black );
+                    vectorVertices[j++] = new VertexPositionColor( new Vector3(0, 0, z), Color.Black );
+
+                    foreach (var pass in effect.CurrentTechnique.Passes)
+                    {
+                        pass.Apply ();
+
+                        graphics.GraphicsDevice.DrawUserPrimitives (
+                            PrimitiveType.LineList,
+                            vectorVertices,
+                            0,
+                            Math.Abs(x) * 2 * 3 + 3
+                        );
+                    }
+                }
+
+            }
+
             public void render()
             {
                 BasicEffect effect = new BasicEffect(graphics.GraphicsDevice);
@@ -40,39 +127,15 @@ namespace Physics_Sim
                 float farClipPlane = 200;
                 effect.Projection = Matrix.CreatePerspectiveFieldOfView(fieldOfView, aspectRatio, nearClipPlane, farClipPlane);
                 
-                //int width = graphics.GraphicsDevice.Viewport.Width;
-                //int height = graphics.GraphicsDevice.Viewport.Height;
-                //float nearClipPlane = 1;
-                //float farClipPlane = 200;
-                //effect.Projection = Matrix.CreateOrthographic(width, height, nearClipPlane, farClipPlane);
-
-                VertexPositionColor[] vectorVertices;
-                vectorVertices = new VertexPositionColor[6];
-                vectorVertices[0].Position = new Vector3(-10, 0, 0);
-                vectorVertices[1].Position = new Vector3(10, 0, 0);
-                vectorVertices[2].Position = new Vector3(0, -10, 0);
-                vectorVertices[3].Position = new Vector3(0, 10, 0);
-                vectorVertices[4].Position = new Vector3(0, 0, -10);
-                vectorVertices[5].Position = new Vector3(0, 0, 10);
-
-                vectorVertices[0].Color = Color.Red;
-                vectorVertices[1].Color = Color.Red;
-                vectorVertices[2].Color = Color.Green;
-                vectorVertices[3].Color = Color.Green;
-                vectorVertices[4].Color = Color.Blue;
-                vectorVertices[5].Color = Color.Blue;
-
-                foreach (var pass in effect.CurrentTechnique.Passes)
-                {
-                    pass.Apply ();
-
-                    graphics.GraphicsDevice.DrawUserPrimitives (
-                        PrimitiveType.LineList,
-                        vectorVertices,
-                        0,
-                        3
-                    );
-                }
+                DrawQuadrent(effect, 5, 5, 5);
+                DrawQuadrent(effect, 5, 5, -5);
+                DrawQuadrent(effect, 5, -5, 5);
+                DrawQuadrent(effect, 5, -5, -5);
+                DrawQuadrent(effect, -5, 5, 5);
+                DrawQuadrent(effect, -5, 5, -5);
+                DrawQuadrent(effect, -5, -5, 5);
+                DrawQuadrent(effect, -5, 5, -5);
+                
             }
         }
     }
